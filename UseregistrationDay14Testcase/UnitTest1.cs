1aef9db8b4f78d4ce5139b85d95c1bc92564d940
sonal <sonalkarle01@gmail.com>
2021-02-25 14:53:28 +0530

    Welcome

--- UseregistrationDay14Testcase/UnitTest1.cs
+++ UseregistrationDay14Testcase/UnitTest1.cs
@@ -1,18 +1,127 @@
 using NUnit.Framework;
+using UserRegistrationLambda;
 
-namespace UseregistrationDay14Testcase
+namespace NUnitTestProject
 {
     public class Tests
     {
+
+        UserRegistrationTestLambda userRegistration;
         [SetUp]
         public void Setup()
         {
+            userRegistration = new UserRegistrationTestLambda();
         }
 
-        [Test]
-        public void Test1()
+        /// <summary>
+        /// TC-1 Throw Custom Exception for Invalid FirstName
+        /// </summary>
+        [TestCase("Sonal")]
+        [TestCase("So")]
+        public void Given_FirstName_Expecting_ThrowCustomException(string firstName)
         {
-            Assert.Pass();
+            
+            try
+            {
+              string  actual = userRegistration.firstNameLambda(firstName);
+            }
+            catch (UserRegistrationTestCustomException exception)
+            {
+                Assert.AreEqual("FirstName should contains atleast three characters", exception.Message);
+            }
         }
+
+        /// <summary>
+        /// TC-2 Throw Custom Exception for Invalid LastName
+        /// </summary>
+        [TestCase("Karle")]
+        [TestCase("Ka")]
+        public void Given_LastName_Expecting_ThrowCustomException(string lastName)
+        {
+            
+            try
+            {
+               string actual = userRegistration.lastNameLambda(lastName);
+            }
+            catch (UserRegistrationTestCustomException exception)
+            {
+                Assert.AreEqual("LastName should contains atleast three characters", exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// TC-3 Throw Custom Exception for Invalid MobileNumber
+        /// </summary>
+        [TestCase("91 9702420754")]
+        [TestCase("")]
+        public void Given_MobileNumber_Expecting_ThrowCustomException(string mobileNumber)
+        {
+            
+            try
+            {
+               string  actual = userRegistration.mobileNumberLambda(mobileNumber);
+            }
+            catch (UserRegistrationTestCustomException exception)
+            {
+                Assert.AreEqual("MobileNumber should not be empty", exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// TC-4 Throw Custom Exception for Invalid Password
+        /// </summary>
+        [TestCase("Sonak451##")]
+        [TestCase("")]
+        public void Given_Password_Expecting_ThrowCustomException(string password)
+        {
+            
+            try
+            {
+               string actual = userRegistration.passwordLambda(password);
+            }
+            catch (UserRegistrationTestCustomException exception)
+            {
+                Assert.AreEqual("Password should not be empty", exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// TC-5 Throw Custom Exception for Invalid Email
+        /// </summary>
+        [TestCase("abc@yahoo.com")]
+        [TestCase("abc-100@yahoo.com,")]
+        [TestCase("abc.100@yahoo.com")]
+        [TestCase("abc111@abc.com,")]
+        [TestCase("abc-100@abc.net,")]
+        [TestCase("abc.100@abc.com.au")]
+        [TestCase("abc@1.com,")]
+        [TestCase("abc@gmail.com.com")]
+        [TestCase("abc+100@gmail.com")]
+        [TestCase("abc")]
+        [TestCase("abc@.com.my")]
+        [TestCase("abc123@gmail.a")]
+        [TestCase("abc123@.com")]
+        [TestCase("abc@.com.com")]
+        [TestCase(".abc@abc.com")]
+        [TestCase("abc()*@gmail.com")]
+        [TestCase("abc@%*.com")]
+        [TestCase("abc..2002@gmail.com")]
+        [TestCase("abc.@gmail.com")]
+        [TestCase("abc@abc@gmail.com")]
+        [TestCase("abc@gmail.com.1a")]
+        [TestCase("abc@gmail.com.aa.au")]
+        public void Given_Email_Expecting_ThrowCustomException(string email)
+        {
+            
+            try
+            {
+              string actual = userRegistration.emailLambda(email);
+            }
+            catch (UserRegistrationTestCustomException exception)
+            {
+                Assert.AreEqual("Email should contains special characters", exception.Message);
+            }
+        }
+
     }
 }
--- UserregistrationDay14/Userregistration.cs
+++ UserregistrationDay14/Userregistration.cs
@@ -1,10 +1,249 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace UserregistrationDay14
+namespace UserRegistrationLambda
 {
-    class Userregistration
+    public class UserRegistrationTestLambda
     {
+        //Regex pattern
+        string firstNameregex = "^[A-Z]{1}[a-z]{2,}$";
+        string lastNameregex = "^[A-Z]{1}[a-z]{2,}$";
+        string emailregex = "^[0-9a-zA-Z]+([._+-]?[0-9a-zA-Z]+)*@[0-9A-Za-z]+.([c]{1}[o]{1}[m]{1})*([n]{1}[e]{1}[t]{1})*[,]*([.][a]{1}[u]{1})*([.][c]{1}[o]{1}[m]{1})*$";
+        string mobileNumberregex = "^[9]{1}[1]{1}[ ][0-9]{10}$";
+        string passwordregex = "^[A-Z]{1}[a-zA-Z]{7,}([0-9]+)[@#$%^&*+-_]{1}$";
+
+        public bool firstName(string patternFirstName) => Regex.IsMatch(patternFirstName, firstNameregex);
+        public bool lastName(string patternLastName) => Regex.IsMatch(patternLastName, lastNameregex);
+        public bool mobileNumber(string patternMobileNumber) => Regex.IsMatch(patternMobileNumber, mobileNumberregex);
+        public bool password(string patternPassword) => Regex.IsMatch(patternPassword, passwordregex);
+        public bool email(string patternEmail) => Regex.IsMatch(patternEmail, emailregex);
+
+
+        /// <summary>
+        /// FirstName Custom Exception
+        /// </summary>
+        /// <param name="patternFirstName"></param>
+        /// <returns></returns>
+        public string firstNameLambda(string patternFirstName)
+        {
+            bool result = firstName(patternFirstName);
+            try
+            {
+                if (result == false)
+                {
+
+                    if (patternFirstName.Equals(string.Empty))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_EMPTY, "FirstName should not be empty");
+                    }
+
+
+                    if (patternFirstName.Length < 3)
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LESSTHAN_MINIMUM_LENGTH, "FirstName should contains atleast three characters");
+
+                    }
+
+                    if (patternFirstName.Any(char.IsDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_NUMBER, "FirstName should not contains number");
+                    }
+                    if (!char.IsUpper(patternFirstName[0]))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LOWERCASE, "FirstName first letter should not be a lowercase");
+                    }
+                    if (patternFirstName.Any(char.IsLetterOrDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_SPECIAL_CHARACTER, "FirstName should not contains special characters");
+                    }
+
+                }
+            }
+
+            catch (UserRegistrationTestCustomException exception)
+            {
+                throw exception;
+            }
+            return "FirstName is not valid";
+        }
+
+        /// <summary>
+        /// LastName Custom Exception
+        /// </summary>
+        /// <param name="patternLastName"></param>
+        /// <returns></returns>
+        public string lastNameLambda(string patternLastName)
+        {
+            bool result = lastName(patternLastName);
+            try
+            {
+                if (result == false)
+                {
+
+                    if (patternLastName.Equals(string.Empty))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_EMPTY, "LastName should not be empty");
+                    }
+
+
+                    if (patternLastName.Length < 3)
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LESSTHAN_MINIMUM_LENGTH, "LastName should contains atleast three characters");
+
+                    }
+
+                    if (patternLastName.Any(char.IsDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_NUMBER, "LastName should not contains number");
+                    }
+                    if (!char.IsUpper(patternLastName[0]))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LOWERCASE, "LastName first letter should not be a lowercase");
+                    }
+                    if (patternLastName.Any(char.IsLetterOrDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_SPECIAL_CHARACTER, "LastName should not contains special characters");
+                    }
+
+                }
+            }
+
+            catch (UserRegistrationTestCustomException exception)
+            {
+                throw exception;
+            }
+            return "LastName is not valid";
+        }
+
+        /// <summary>
+        /// MobileNumber Custom Exception
+        /// </summary>
+        /// <param name="patternMobileNumber"></param>
+        /// <returns></returns>
+        public string mobileNumberLambda(string patternMobileNumber)
+        {
+            bool result = mobileNumber(patternMobileNumber);
+            try
+            {
+                if (result == false)
+                {
+
+                    if (patternMobileNumber.Equals(string.Empty))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_EMPTY, "MobileNumber should not be empty");
+                    }
+
+
+                    if (patternMobileNumber.Length < 13)
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LESSTHAN_MINIMUM_LENGTH, "MobileNumber should contains thirteen characters");
+
+                    }
+
+                    if (patternMobileNumber.Any(char.IsLetter))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_NUMBER, "MobileNumber should not contains letters");
+                    }
+
+                    if (patternMobileNumber.Any(char.IsLetterOrDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_SPECIAL_CHARACTER, "MobileNumber should not contains special characters");
+                    }
+
+                }
+            }
+
+            catch (UserRegistrationTestCustomException exception)
+            {
+                throw exception;
+            }
+            return "MobileNumber is not valid";
+        }
+
+        /// <summary>
+        /// Password Custom Exception
+        /// </summary>
+        /// <param name="patternPassword"></param>
+        /// <returns></returns>
+        public string passwordLambda(string patternPassword)
+        {
+            bool result = password(patternPassword);
+            try
+            {
+                if (result == false)
+                {
+
+                    if (patternPassword.Equals(string.Empty))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_EMPTY, "Password should not be empty");
+                    }
+
+
+                    if (patternPassword.Length < 8)
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LESSTHAN_MINIMUM_LENGTH, "Password should contains atleast eight characters");
+
+                    }
+
+                    if (!patternPassword.Any(char.IsDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_NUMBER, "Password should contains number");
+                    }
+                    if (!char.IsUpper(patternPassword[0]))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_LOWERCASE, "Password first letter should not be a lowercase");
+                    }
+                    if (!patternPassword.Any(char.IsLetterOrDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_SPECIAL_CHARACTER, "Password should contains special characters");
+                    }
+
+                }
+            }
+
+            catch (UserRegistrationTestCustomException exception)
+            {
+                throw exception;
+            }
+            return "Password is not valid";
+        }
+
+        /// <summary>
+        /// Email Custom Exception
+        /// </summary>
+        /// <param name="patternEmail"></param>
+        /// <returns></returns>
+        public string emailLambda(string patternEmail)
+        {
+            bool result = email(patternEmail);
+            try
+            {
+                if (result == false)
+                {
+
+                    if (patternEmail.Equals(string.Empty))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_EMPTY, "Email should not be empty");
+                    }
+                    if (!patternEmail.Any(char.IsLetterOrDigit))
+                    {
+                        throw new UserRegistrationTestCustomException(UserRegistrationTestCustomException.ExceptionType.ENTERED_SPECIAL_CHARACTER, "Email should contains special characters");
+                    }
+
+                }
+            }
+
+            catch (UserRegistrationTestCustomException exception)
+            {
+                throw exception;
+            }
+            return "Email is not valid";
+        }
+
+
+
     }
 }
